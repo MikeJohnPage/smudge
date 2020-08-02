29f1948252003e061175fd9765be3c93ac013c10
Mike Page <38110953+MikeJohnPage@users.noreply.github.com>
2020-08-02 21:47:51 +0200

    Smudge addresses

---
+++ R/smudge_addresses.R
@@ -0,0 +1,20 @@
+smudge_addresses <- function(vec, replace_na = TRUE){
+
+  # Check arg types
+  stopifnot(is.character(vec))
+  stopifnot(is.logical(replace_na))
+
+  # Remove addresses else replace with NA
+  if(replace_na == TRUE){
+    vec[grepl(pattern = "(?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\\.?",
+              x = vec,
+              perl = TRUE)] = NA
+  } else{
+    vec <- vec[!grepl(pattern = "(?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\\.?",
+                      x = vec,
+                      perl = TRUE)]
+  }
+
+  return(vec)
+
+}
